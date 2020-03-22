038469349efc20753977a4f82d72894b2173e58f
Arnuel Gutierrez Menco <argume31@gmail.com>
2020-03-22 18:49:09 -0500

    Add css animation to enter and leave item

--- demo/index.js
+++ demo/index.js
@@ -1,9 +1,9 @@
 const tasksApp = new Do('task_container', 'form_task', {
-    'itemClass': 'flex items-center border-2 rounded-bl-lg rounded-tr-lg border-indigo-700 px-4 py-3 mb-3 hover:shadow-lg',
-    'checkItemClass': 'item__check mr-4 cursor-pointer appearance-none border w-6 h-6 rounded-full flex items-center justify-center outline-none',
-    'contentItemClass': 'flex-grow text-indigo-700 item__content',
-    'deleteItemButtonClass': 'ml-4 text-sm text-gray-500 cursor-pointer',
-    'completedItemClass': 'item__completed',
+    'itemClass'             : 'item--enter flex items-center border-2 rounded-bl-lg rounded-tr-lg border-indigo-700 px-4 py-3 mb-3 shadow-lg',
+    'checkItemClass'        : 'item__check mr-4 cursor-pointer appearance-none border w-6 h-6 rounded-full flex items-center justify-center outline-none',
+    'contentItemClass'      : 'flex-grow text-indigo-700 item__content',
+    'deleteItemButtonClass' : 'ml-4 text-sm text-gray-500 cursor-pointer',
+    'deleteDelay'           : 300
 })
 
 // Create item on press enter key
