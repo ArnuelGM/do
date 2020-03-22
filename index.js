c7557a887a39f51301c93a6bcc0c1689a5ae2438
Arnuel Gutierrez Menco <argume31@gmail.com>
2020-03-22 10:51:34 -0500

    Change custom css by tailwindcss :: Add custom itemValidator

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 const tasksApp = new Do('task_container', 'form_task', {
-    deleteDelay: 300
+    'itemClass': 'flex items-center border-2 rounded-bl-lg rounded-tr-lg border-indigo-700 px-4 py-3 mb-3 hover:shadow-lg',
+    'checkItemClass': 'item__check mr-4 cursor-pointer appearance-none border w-6 h-6 rounded-full flex items-center justify-center outline-none',
+    'contentItemClass': 'flex-grow text-indigo-700 item__content',
+    'deleteItemButtonClass': 'ml-4 text-sm text-gray-500 cursor-pointer',
+    'completedItemClass': 'item__completed',
 })
+
+// Create item on press enter key
+tasksApp.form.input.addEventListener('keyup', (ev) => {
+    if(ev.keyCode === 13) {
+        tasksApp.form.createNewItem()
+    }
+})
+
+// Custom item validator
+tasksApp.form.itemValidator = (item) => {
+    if( item.name.trim().length == 0 ) {
+        alert('Jajaja que dijiste, CORONÉ??')
+        return false
+    }
+    return true
+}
+
 tasksApp.init()
